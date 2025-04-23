8a7000bb109235b51b72f5970b349813ee4d3808
Tanel Tammet <tanel.tammet@gmail.com>
2025-04-23 13:39:04 +0300

    first commit

---
+++ llmpipe/nlpsimpletext.py
@@ -0,0 +1,56 @@
+[[
+  """List of cities and towns in Estonia:
+
+Rank	Town	Town rights	Population 2014[2]	Population 2018[3]	County (maakond)
+1	Tallinn	1248	411,063	430,805	Harju
+2	Tartu	13th c.	98,449	93,715	Tartu
+3	Narva	1345	59,049	56,103	Ida-Viru
+4	Pärnu[a]	1251	40,005	39,375	Pärnu
+5	Kohtla-Järve	1946	37,198	34,394	Ida-Viru
+6	Viljandi	1283	17,602	17,525	Viljandi
+7	Rakvere	1302	15,400	15,413	Lääne-Viru
+8	Maardu	1980	17,315	15,189	Harju
+9	Kuressaare[a]	1563	13,152	13,276	Saare
+10	Sillamäe	1957	14,122	12,989	Ida-Viru
+11	Valga[a]	1584	12,437	12,334	Valga
+12	Võru	1784	12,571	12,022	Võru
+13	Jõhvi[a]	1938	10,525	10,398	Ida-Viru
+14	Haapsalu[a]	1279	10,316	9,838	Lääne
+15	Keila	1938	9,751	9,775	Harju
+16	Paide[a]	1291	8,162	8,078	Järva
+17	Elva[a]	1938	5,657	5,648	Tartu
+18	Saue[a]	1993	5,656	5,640	Harju
+19	Põlva[a]	1993	5,661	5,458	Põlva
+20	Tapa[a]	1926	5,693	5,428	Lääne-Viru
+21	Jõgeva[a]	1938	5,457	5,143	Jõgeva
+22	Rapla[a]	1993	5,054	5,132	Rapla
+23	Kiviõli[a]	1946	5,635	5,088	Ida-Viru
+24	Türi[a]	1926	5,215	5,064	Järva
+25	Põltsamaa[a]	1926	4,148	4,050	Jõgeva""",True],
+[
+  """List of cities and towns in Estonia:
+
+Rank	Town	Town rights	Population 2014[2]	Population 2018[3]	County (maakond)
+26	Sindi[a]	1938	4,089	3,838	Pärnu
+27	Paldiski[a]	1783	4,163	3,626	Harju
+28	Kärdla[a]	1938	3,017	3,287	Hiiu
+29	Kunda[a]	1938	3,267	3,034	Lääne-Viru
+30	Tõrva[a]	1926	2,716	2,740	Valga
+31	Narva-Jõesuu[a]	1993	2,655	2,644	Ida-Viru
+32	Kehra[a]	1993	2,792	2,641	Harju
+33	Loksa	1993	2,703	2,568	Harju
+34	Räpina[a]	1993	2,363	2,173	Põlva
+35	Otepää[a]	1936	1,875	2,167	Valga
+36	Tamsalu[a]	1996	2,174	2,119	Lääne-Viru
+37	Kilingi-Nõmme[a]	1938	1,707	1,671	Pärnu
+38	Karksi-Nuia[a]	1993	1,515	1,554	Viljandi
+39	Antsla[a]	1938	1,362	1,311	Võru
+40	Võhma[a]	1993	1,282	1,282	Viljandi
+41	Mustvee[a]	1938	1,348	1,269	Jõgeva
+42	Lihula[a]	1993	1,252	1,214	Lääne
+43	Suure-Jaani[a]	1938	1,036	1,064	Viljandi
+44	Abja-Paluoja[a]	1993	1,063	1,057	Viljandi
+45	Püssi[a]	1993	1,023	917	Ida-Viru
+46	Mõisaküla[a]	1938	806	748	Viljandi
+47	Kallaste[a]	1938	846	743	Tartu""",True]]
+
